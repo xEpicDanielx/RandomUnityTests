0a67f4ca34ef0a9ea82aaab78ea5d2984147c229
Daniel Marifjeren <marifjeren1@yahoo.com>
2018-10-13 12:39:17 -0400

    Added qhortcut to disable objects
    added 1 more ui item. also added hotkey q to disable/enable selected object

---
+++ DragonBallZPowerUpApp/Assets/Scripts/Editor/myShortcuts.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MyShortcuts : Editor
+{
+    [MenuItem("GameObject/ActiveToggle _q")]
+    static void ToggleActivationSelection()
+    {
+        var go = Selection.activeGameObject;
+        go.SetActive(!go.activeSelf);
+    }
+}
